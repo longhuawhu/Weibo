e98be3b32375be445a0b8d2b1b44ad3fc7f55e7c
longhuawhu <longhua1949hm@163.com>
2015-02-02 10:58:30 +0800

    Initial Commit

---
+++ Weibo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Weibo
+//
+//  Created by lh on 15/2/2.
+//  Copyright (c) 2015年 lh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Weibo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Weibo
+//
+//  Created by lh on 15/2/2.
+//  Copyright (c) 2015年 lh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
